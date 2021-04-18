87d34fb22812b6995995ede297807743e02a9b7b
trantvivian <trantvivian@gmail.com>
2021-04-18 15:08:32 -0400

    all starter pages are in

--- navbar.php
+++ navbar.php
@@ -42,7 +42,7 @@
                     <a class="nav-link" href="profile.php">Profile</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact Us</a>
+                    <a class="nav-link" href="contact.php">Contact Us</a>
                 </li>
                 <!-- LOG IN BUTTON, REDIRECTS TO LOGIN PAGE -->
                 <?php
