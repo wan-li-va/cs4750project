824d0407fd251d634972aaf659141a2b0bc9fe27
trantvivian <trantvivian@gmail.com>
2021-04-18 15:06:34 -0400

    favorites.php

---
+++ favorites.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- required to handle IE -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <!-- ICON  -->
+    <link rel="shortcut icon" href="https://media2.giphy.com/media/n9wqJ8gTR9lQnXTvf3/giphy_s.gif" type="image/ico" />
+    <!-- EXTERNAL CSS -->
+    <link href="./styles/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<?php include "./navbar.php" ?>
+
+<body>
+    <body>
+          <div class="jumbotron">
+            <div class="container">
+                <center>
+              <h1 class="display-4">Pets Available for Adoption Today</h1>
+              <a class="btn btn-lg" href="" role="button">Find a Dog</a>
+              <a class="btn btn-lg" href="" role="button">Find a Cat</a>
+                </center>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-sm" style="text-align:center">
+                <img src="https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=ZhplzyJ9" style="width:300px; border:10px solid #efc164">
+                <h1 class="display-6">Bell</h1>
+                <a href="">Learn More</a>
+                </div>
+                <div class="col-sm" style="text-align:center">
+                <img src="https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=ZhplzyJ9" style="width:300px; border:10px solid #efc164">
+                <h1 class="display-6">Bell</h1>
+                <a href="">Learn More</a>
+                </div>
+                <div class="col-sm" style="text-align:center">
+                <img src="https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=ZhplzyJ9" style="width:300px; border:10px solid #efc164">
+                <h1 class="display-6">Bell</h1>
+                <a href="">Learn More</a>
+                </div>
+            </div>
+          </div>
+    </body>
+</body>
