a70e884b5421f54324716c3de246e3941a153ebd
Wan Li <wl9wgc@virginia.edu>
2021-05-02 13:03:36 -0400

    filter pls

--- pets.php
+++ pets.php
@@ -10,8 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['pName'] = $_POST['pName'];
         $_SESSION['pDOB'] = $_POST['pDOB'];
         header("Location: petUpdate.php");
-    }
-    elseif (!empty($_POST['action']) && ($_POST['action'] == 'Apply to Adopt')) {
+    } elseif (!empty($_POST['action']) && ($_POST['action'] == 'Apply to Adopt')) {
         $username = $_SESSION['user'];
         $name = $_POST['pName'];
         $dob = $_POST['pDOB'];
@@ -27,7 +26,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 alert('Application sent');
                 window.location.href='pets.php';
                 </script>";
-        
     }
     if (!empty($_POST['action']) && ($_POST['action'] == 'Favorite')) {
         $username = $_SESSION['user'];
@@ -95,6 +93,33 @@ if (isset($_SESSION['user'])) {
         <div class="sketchy">
             <h1 class="title">Pets Available </h1>
         </div>
+
+        <div class='pet-type' style='display: flex; flex-direction: row;'>
+            <p style='margin-right: 2vw;'>Filter for: </p>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method='get'>
+                <div class="form-check" style='margin-right: 1vw;'>
+                    <input class="form-check-input" type="radio" name="pettype" value='any' id="pettype" checked>
+                    <label class="form-check-label" for="any">
+                        Any
+                    </label>
+                </div>
+                <div class="form-check" style='margin-right: 1vw;'>
+                    <input class="form-check-input" type="radio" name="pettype" value='dogs' id="pettype">
+                    <label class="form-check-label" for="dogs">
+                        Dogs
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="pettype" value='cats' id="pettype">
+                    <label class="form-check-label" for="cats">
+                        Cats
+                    </label>
+                </div>
+                <input type="hidden" name="type" value="document.getElementById('pettype').value" />
+                <input class="btn btn-primary" type="submit" value="choose" name="action" />
+            </form>
+        </div>
+
         <?php
         if ($loggedIn)
             echo "<div style='margin-bottom: 3vh;'><a class='btn btn-lg' href='favorites.php' style='width: 15vw;'> Your Favorite Pets </a>";
@@ -111,157 +136,177 @@ if (isset($_SESSION['user'])) {
         <br>
 
         <?php
-        // Form the SQL query (a SELECT query)
+        // Form the SQL query (a SELECT query) 
 
-
+        $type = 'any';
         $query = "SELECT * FROM pets ORDER BY name";
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $pets_info = $statement->fetchAll();
-        $statement->closecursor();
+        display($db, $query, $manager, $loggedIn);
 
-        $output = "<h2>Welcome to Modern Business
+        echo $_GET['type'];
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            if (!empty($_POST['action'])) {
+                $type = $_GET['type'];
+                if ($type == 'any') {
+                    $query = "SELECT * FROM pets ORDER BY name";
+                } else if ($type == 'dogs') {
+                    $query = "SELECT * FROM pets WHERE type_of_animal='dogs' ORDER BY name";
+                } else {
+                    $query = "SELECT * FROM pets WHERE type_of_animal='cats' ORDER BY name";
+                }
+                display($db, $query, $manager, $loggedIn);
+                echo "<script> window.location.href='favorites.php'; </script>";
+            }
+        }
+
+
+        function display($db, $query, $manager, $loggedIn)
+        {
+            $statement = $db->prepare($query);
+            $statement->execute();
+            $pets_info = $statement->fetchAll();
+            $statement->closecursor();
+
+            $output = "<h2>Welcome to Modern Business
             <div class='container'>
             <div class='row'>";
 
-        echo "<div class='row'>";
-        foreach ($pets_info as $row) {
-            // $name = $row['name'];
-            // $desciption = $row['desciption'];
-            // $ephoto = $row['ephoto'];
-            echo "<div class='col-sm-3'>";
-            echo  "<div class='card' style='height: 110vh; width: 21vw; margin-bottom: 5vh;'>";
-            echo  "<div class='card-body'>";
+            echo "<div class='row'>";
+            foreach ($pets_info as $row) {
+                // $name = $row['name'];
+                // $desciption = $row['desciption'];
+                // $ephoto = $row['ephoto'];
+                echo "<div class='col-sm-3'>";
+                echo  "<div class='card' style='height: 110vh; width: 21vw; margin-bottom: 5vh;'>";
+                echo  "<div class='card-body'>";
 
-            if ($row['image'] != NULL) {
-                echo "<img class='animalimg' img src='";
-                echo $image = $row['image'];
-                echo "' style='height: 15vh; margin-bottom: 2vh;'> </img>";
-            } else {
-                echo "<img class='animalimg' style='height: 15vh; margin-bottom: 2vh;' 
+                if ($row['image'] != NULL) {
+                    echo "<img class='animalimg' img src='";
+                    echo $image = $row['image'];
+                    echo "' style='height: 15vh; margin-bottom: 2vh;'> </img>";
+                } else {
+                    echo "<img class='animalimg' style='height: 15vh; margin-bottom: 2vh;' 
                 img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png'> </img>";
-            }
+                }
 
-            echo "<h1 style='text-align:center;'>";
-            echo  $name =  $row['name'];
-            echo "</h1>";
+                echo "<h1 style='text-align:center;'>";
+                echo  $name =  $row['name'];
+                echo "</h1>";
 
-            //Birthdate
-            echo "<p class='petsinfo'> <b> Birthdate (YYYY-MM-DD): </b>";
-            echo $dob = $row['dob'];
-            echo "</p>";
+                //Birthdate
+                echo "<p class='petsinfo'> <b> Birthdate (YYYY-MM-DD): </b>";
+                echo $dob = $row['dob'];
+                echo "</p>";
 
-            //Sex, ASSUME THE LETTER IS CAPITALIZED
-            echo "<p class='petsinfo'> <b> Sex: </b>";
-            if ($row['sex'] == "F") {
-                echo "Female";
-            } else {
-                echo "Male";
-            }
-            echo "</p>";
+                //Sex, ASSUME THE LETTER IS CAPITALIZED
+                echo "<p class='petsinfo'> <b> Sex: </b>";
+                if ($row['sex'] == "F") {
+                    echo "Female";
+                } else {
+                    echo "Male";
+                }
+                echo "</p>";
 
-            //Type of Animal
-            echo "<p class='petsinfo'> <b> Animal: </b>";
-            echo $type = $row['type_of_animal'];
-            echo "</p>";
+                //Type of Animal
+                echo "<p class='petsinfo'> <b> Animal: </b>";
+                echo $type = $row['type_of_animal'];
+                echo "</p>";
 
-            //Color
-            echo "<p class='petsinfo'> <b> Color: </b>";
-            echo $color = $row['color'];
-            echo "</p>";
+                //Color
+                echo "<p class='petsinfo'> <b> Color: </b>";
+                echo $color = $row['color'];
+                echo "</p>";
 
-            //Breed
-            echo "<p class='petsinfo'> <b> Breed: </b>";
-            echo $breed = $row['breed'];
-            echo "</p>";
-
-
-            // Vaccinations
-            echo "<p class='petsinfo'> <b> Vaccinations: </b>";
-            if ($row['is_vaccinated'] == 1) {
-                echo "Up to Date";
-            } else {
-                echo "Missing";
-            }
-            echo "</p>";
-
-            //spayed/neutered
-            echo "<p class='petsinfo'> <b>Spayed/Neutered: </b>";
-            if ($row['is_spayed_neutered'] == 1) {
-                echo "Yes";
-            } else {
-                echo "No";
-            }
-            echo "</p>";
+                //Breed
+                echo "<p class='petsinfo'> <b> Breed: </b>";
+                echo $breed = $row['breed'];
+                echo "</p>";
 
 
-            //shelter
-            echo "<p class='petsinfo'> <b>Current Shelter: </b>";
-            echo $shelter = $row['shelter_name'];
-            echo "</p>";
+                // Vaccinations
+                echo "<p class='petsinfo'> <b> Vaccinations: </b>";
+                if ($row['is_vaccinated'] == 1) {
+                    echo "Up to Date";
+                } else {
+                    echo "Missing";
+                }
+                echo "</p>";
 
-            //adoptable
-            echo "<p class='petsinfo'> <b>Adoptable: </b>";
-            if ($row['is_adoptable'] == 1) {
-                echo "Up for Adoption";
-            } else {
-                echo "Not Up for Adoption";
-            }
-            echo "</p>";
+                //spayed/neutered
+                echo "<p class='petsinfo'> <b>Spayed/Neutered: </b>";
+                if ($row['is_spayed_neutered'] == 1) {
+                    echo "Yes";
+                } else {
+                    echo "No";
+                }
+                echo "</p>";
 
 
-            //fosterable
-            echo "<p class='petsinfo'> <b>Fosterable: </b>";
-            if ($row['is_fosterable'] == 1) {
-                echo "Up for Foster";
-            } else {
-                echo "Not Fosterable";
-            }
-            echo "</p>";
+                //shelter
+                echo "<p class='petsinfo'> <b>Current Shelter: </b>";
+                echo $shelter = $row['shelter_name'];
+                echo "</p>";
 
-            //extra notes
-            echo "<p class='petsinfo'> <b>Extra notes: </b>";
-            echo $notes = $row['notes'];
-            echo "</p>";
+                //adoptable
+                echo "<p class='petsinfo'> <b>Adoptable: </b>";
+                if ($row['is_adoptable'] == 1) {
+                    echo "Up for Adoption";
+                } else {
+                    echo "Not Up for Adoption";
+                }
+                echo "</p>";
 
-            echo "<div class='card-buttons' style='display: flex; flex-direction: row; position: absolute; bottom: 5vh;'>";
-            //pet edit
-            if ($manager) {
-                echo "<p class='petsinfo'>";
-                echo '<form action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '"method="post">';
-                echo "<div style='text-align:center; margin-right: 1vw;'>";
-                echo '<input type="submit" value="Update" name="action" class="btn btn-primary" />';
-                echo '<input type="hidden" name="pName" value="' . $row['name'] . '" />';
-                echo '<input type="hidden" name="pDOB" value="' . $row['dob'] . '" />';
+
+                //fosterable
+                echo "<p class='petsinfo'> <b>Fosterable: </b>";
+                if ($row['is_fosterable'] == 1) {
+                    echo "Up for Foster";
+                } else {
+                    echo "Not Fosterable";
+                }
+                echo "</p>";
+
+                //extra notes
+                echo "<p class='petsinfo'> <b>Extra notes: </b>";
+                echo $notes = $row['notes'];
+                echo "</p>";
+
+                echo "<div class='card-buttons' style='display: flex; flex-direction: row; position: absolute; bottom: 5vh;'>";
+                //pet edit
+                if ($manager) {
+                    echo "<p class='petsinfo'>";
+                    echo '<form action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '"method="post">';
+                    echo "<div style='text-align:center; margin-right: 1vw;'>";
+                    echo '<input type="submit" value="Update" name="action" class="btn btn-primary" />';
+                    echo '<input type="hidden" name="pName" value="' . $row['name'] . '" />';
+                    echo '<input type="hidden" name="pDOB" value="' . $row['dob'] . '" />';
+                    echo "</div>";
+                    echo "</form>";
+                } elseif ($loggedIn) {
+                    echo "<p class='petsinfo'>";
+                    echo '<form action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '"method="post">';
+                    echo "<div style='text-align:center; margin-right: 1vw;'>";
+                    echo '<input type="submit" value="Apply to Adopt" name="action" class="btn btn-primary" />';
+                    echo '<input type="hidden" name="pName" value="' . $row['name'] . '" />';
+                    echo '<input type="hidden" name="pDOB" value="' . $row['dob'] . '" />';
+                    echo "</div>";
+                    echo "</form>";
+                }
+                if ($loggedIn) {
+                    echo '<form action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '"method="POST">';
+                    echo '<input class="btn btn-primary" type="submit" value="Favorite" name="action" />';
+                    echo '<input type="hidden" name="pName2" value="' . $row['name'] . '" />';
+                    echo '<input type="hidden" name="pDOB2" value="' . $row['dob'] . '" />';
+                    echo '<input type="hidden" name="pImg2" value="' . $row['image'] . '" />';
+                    echo "</form>";
+                }
                 echo "</div>";
-                echo "</form>";
-            }
-            elseif($loggedIn)
-            {
-                echo "<p class='petsinfo'>";
-                echo '<form action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '"method="post">';
-                echo "<div style='text-align:center; margin-right: 1vw;'>";
-                echo '<input type="submit" value="Apply to Adopt" name="action" class="btn btn-primary" />';
-                echo '<input type="hidden" name="pName" value="' . $row['name'] . '" />';
-                echo '<input type="hidden" name="pDOB" value="' . $row['dob'] . '" />';
-                echo "</div>";
-                echo "</form>";
-            }
-            if ($loggedIn) {
-                echo '<form action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '"method="POST">';
-                echo '<input class="btn btn-primary" type="submit" value="Favorite" name="action" />';
-                echo '<input type="hidden" name="pName2" value="' . $row['name'] . '" />';
-                echo '<input type="hidden" name="pDOB2" value="' . $row['dob'] . '" />';
-                echo '<input type="hidden" name="pImg2" value="' . $row['image'] . '" />';
-                echo "</form>";
-            }
-            echo "</div>";
 
-            echo  "</div>";
-            echo  "</div>";
+                echo  "</div>";
+                echo  "</div>";
+                echo  "</div>";
+            }
             echo  "</div>";
         }
-        echo  "</div>";
         // $name =  $row['name'];
         //     $dob = $row['dob'];
         //     $sex =  $row['sex'] ;
